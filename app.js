a9d0b6f18e78d75b166704ac739358d4781aa8f5
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-11-03 09:33:24 +0530

    all tasks done except delete functionality

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-const sequelize=require('./util/database')
-
-const bookingfromRoute=require('./Routes/bookform');
+const sequelize = require('./util/database');
+const bookingfromRoute = require('./Routes/bookform');
+const User = require('./Models/Users');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/',bookingfromRoute)
-app.post('/register', async (req, res) => {
-    try {
-      const connection = await mysql.createConnection(dbConfig);
-      const [results] = await connection.execute(
-        'INSERT INTO users (username, email) VALUES (?, ?)',
-        [req.body.username, req.body.email]
-      );
-      await connection.end();
-      res.send('User registered successfully!');
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Registration failed');
-    }
-  });
-sequelize.sync().then((result)=>{
-    //console.log(result);
-    app.listen(3000);
-}).catch((err)=>{
-    console.log(err)
-})
+app.use('/', bookingfromRoute);
+
+app.post('/user/add-user', async (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const phoneno = req.body.phoneno;
+
+  const data = await User.create({ name: name, email: email, phoneno: phoneno });
+  res.redirect('/')
+  //res.status(201).json({ newUserDetail: data });
+});
+
+app.get('/user/get-user', async (req, res, next) => {
+  const users = await User.findAll();
+  res.status(200).json({ allUsers: users });
+});
+
+
+sequelize.sync().then((result) => {
+  app.listen(3000);
+}).catch((err) => {
+  console.log(err);
+});
+
